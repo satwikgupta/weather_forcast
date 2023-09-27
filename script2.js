591e0414b7d7917e9954b24e0b203d6a1df33911
Satwik Gupta <satwikgupta1801@gmail.com>
2023-09-27 09:28:01 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,46 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b7c88887a4msh1e2048ba063a89ep189089jsnfefb715af240',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+
+async function fetchData(city) {
+	try {
+		cityname.innerHTML = city
+		const response = await fetch(url + city, options);
+		const result = await response.json();
+		console.log(result);
+
+		CO_concentration.innerHTML = result.CO.concentration
+		NO2_concentration.innerHTML = result.NO2.concentration
+		O3_concentration.innerHTML = result.O3.concentration
+		SO2_concentration.innerHTML = result.SO2.concentration
+		PM2_concentration.innerHTML = result["PM2.5"].concentration
+		PM10_concentration.innerHTML = result.PM10.concentration
+		
+		
+		CO_aqi.innerHTML = result.CO.aqi
+		NO2_aqi.innerHTML = result.NO2.aqi
+		O3_aqi.innerHTML = result.O3.aqi
+		SO2_aqi.innerHTML = result.SO2.aqi
+		PM2_aqi.innerHTML = result["PM2.5"].aqi
+		PM10_aqi.innerHTML = result.PM10.aqi
+		Overall_aqi.innerHTML = result.overall_aqi
+	  
+
+
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+submit.addEventListener('click', (e)=>{
+	e.preventDefault();
+	fetchData(city.value);
+})
+
+fetchData("Delhi");
